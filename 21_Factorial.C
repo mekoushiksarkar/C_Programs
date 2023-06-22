560ad008652f17e2971f398cafa8b152767be673
Koushik Sarkar <135224594+mekoushiksarkar@users.noreply.github.com>
2023-06-22 23:40:29 +0530

    Factorial of a Number

---
+++ 21_Factorial.C
@@ -0,0 +1,19 @@
+/* To find the Factorial of a Number */
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,n,fact;
+    printf("\n Enter the number to find Factorial: ");
+    scanf("%d",&n);
+    i=n;
+    fact=1;
+    while(i>0)
+    {
+        fact=fact*i;
+        i--;
+    }
+    printf("\n The factorial of %d :  %d",n,fact);
+    return 0;
+}
