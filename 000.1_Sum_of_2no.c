b11eab6b6373d14101c6c2add26df4400a8997b7
Koushik Sarkar <135224594+mekoushiksarkar@users.noreply.github.com>
2023-12-01 10:12:32 +0530

    Sum of Two Numbers

--- 000.1_Sum_of_2no.c
+++ 000.1_Sum_of_2no.c
@@ -1,9 +1,12 @@
-# include <stdio.h>
-# include<conio.h>
+/* Sum of Two No. */
+
+#include <stdio.h>
+#include <conio.h>
+
 int main()
 {
-    int a=2,b=3,c;
-    c=a^b;
-    printf("\n c=%d",c);
+    int a = 2, b = 3, c;
+    c = a + b;
+    printf("\n Sum of Two No. (c) = %d", c);
     return 0;
 }
