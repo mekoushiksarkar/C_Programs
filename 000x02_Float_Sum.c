50ceb6f4a3b48f43b590f371815eaccb8d3d1a73
Koushik Sarkar <135224594+mekoushiksarkar@users.noreply.github.com>
2023-12-15 13:05:46 +0530

    Sum of Two Floating Numbers

---
+++ 000x02_Float_Sum.c
@@ -0,0 +1,15 @@
+/* Sum of Two Floating Numbers */
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    float a, b, c;
+    printf("\n Enter 1st Number: ");
+    scanf("%f", &a);
+    printf("\n Enter 2nd Number: ");
+    scanf("%f", &b);
+    c = a + b;
+    printf("\n Sum: %.2f", c);
+    return 0;
+}
