3a8106022fc08984d2d34c397e9540d0a71f9400
Koushik Sarkar <135224594+mekoushiksarkar@users.noreply.github.com>
2023-12-21 23:35:16 +0530

    Palindrome Numbers beteen Range

---
+++ 015x01_Palindrome_in_Range.c
@@ -0,0 +1,27 @@
+/* Palindrome Numbers beteen Range */
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int n, s, r, i, t, ran;
+    printf("\n Enter the range to check palindrome number: ");
+    scanf("%d", &ran);
+    t = n;
+    printf("\n Palindrome numbers are: ");
+    for (i = 1; i < ran; i++)
+    {
+        n = i;
+        t = n;
+        s = 0;
+        while (n > 0)
+        {
+            r = n % 10;
+            s = (s * 10) + r;
+            n = n / 10;
+        }
+        if (t == s)
+            printf(" %4d ", t);
+    }
+    return 0;
+}
