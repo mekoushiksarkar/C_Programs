8579918c8d553a60cb2967254db26e16a109ec9e
Koushik Sarkar <135224594+mekoushiksarkar@users.noreply.github.com>
2024-02-04 19:58:47 +0530

    Basic String Print

---
+++ 070x01_Basic_String.c
@@ -0,0 +1,14 @@
+/* Basic String Print */
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    char s1[15];
+    printf("Enter a string: ");
+    gets(s1);
+    printf("Your entered string is: ");
+    puts(s1);
+    return 0;
+}
